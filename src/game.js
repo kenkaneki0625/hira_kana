bafcb4366024ce8f6c030d91b6d4ffe4cf3ffcdc
Shaon Baidya <kenkaneki0625@gmail.com>
2021-10-27 18:08:17 +0530

    card click logic and css update

--- src/game.js
+++ src/game.js
@@ -2,17 +2,59 @@ class MixOrMatch {
   constructor(totalTime, cards) {
       this.cardsArray = cards;
       this.totalTime = totalTime;
+      this.cardToCheck = null;
+      this.matchedCards = [];
   }
-
   startGame() {
     this.shuffleCards(this.cardsArray);
   }
+
   shuffleCards(cardsArray) { // Fisher-Yates Shuffle Algorithm.
       for (let i = cardsArray.length - 1; i > 0; i--) {
           let randIndex = Math.floor(Math.random() * (i + 1));
           cardsArray[randIndex].style.order = i;
           cardsArray[i].style.order = randIndex;
       }
+  }
+
+  flipCard(card) {
+    if(this.canFlipCard(card)) {
+      card.classList.add('click');
+
+        if(this.cardToCheck) {
+            this.checkForCardMatch(card);
+        } else {
+            this.cardToCheck = card;
+        }
+    }
+  }
+  checkForCardMatch(card) {
+    if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+        this.cardMatch(card, this.cardToCheck);
+    else 
+        this.cardMismatch(card, this.cardToCheck);
+
+    this.cardToCheck = null;
+  }
+  cardMatch(card1, card2) {
+    this.matchedCards.push(card1);
+    this.matchedCards.push(card2);
+    card1.classList.remove('click');
+    card2.classList.remove('click');
+    card1.classList.add('matched');
+    card2.classList.add('matched');
+  }
+  cardMismatch(card1, card2) {
+    card1.classList.remove('click');
+    card2.classList.remove('click');
+  }
+
+  getCardType(card) {
+    return card.getElementsByClassName('card-value')[0].id;
+  }
+
+  canFlipCard(card) {
+    return !this.matchedCards.includes(card) && card !== this.cardToCheck;
   }
 }
 
@@ -29,7 +71,7 @@ function ready() {
 
   cards.forEach(card => {
       card.addEventListener('click', () => {
-          console.log(card)
+          game.flipCard(card);
       });
   });
 }
